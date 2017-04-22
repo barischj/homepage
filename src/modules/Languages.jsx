2a07fb6dabc44b4ce75622518ebfa88f0a03451c
Jeremy <barischj@tcd.ie>
2017-04-22 18:15:25 +0100

    Add key to React list elements

--- src/modules/Languages.jsx
+++ src/modules/Languages.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { map } from 'ramda';
 import colours from 'Src/language-colours.json';
 
-// A coloured language and LOC information.
+/**
+ * @param {[string, number]} args - A tuple of a language and its percentage of
+ *   use.
+ * @returns {object} A React div element showing a language and its percentage
+ *   of use, coloured based on the language.
+ */
 function renderLanguage(args) {
-  const [language, loc] = args;
+  const [language, percent] = args;
   return (
-    <div className="language" style={{ backgroundColor: colours[language] }}>
+    <div
+      className="language"
+      key={`${language}${percent}`}
+      style={{ backgroundColor: colours[language] }}
+    >
       <span>{language}</span>
-      <span>{`${loc} %`}</span>
+      <span>{`${percent} %`}</span>
     </div>
   );
 }
